d498e7fb00c5ac477debeecdb61b48eec16c0c02
Rafael Kataoka <rafaelktk2005@gmail.com>
2026-06-03 05:11:17 -0300

    url bug

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 //  CONFIGURAÇÃO — altere a URL base aqui
 //  quando for conectar ao backend
 // =============================================
-const BASE_URL = "http://cadastro-usuario-production.up.railway.app";
+const BASE_URL = "https://cadastro-usuario-production.up.railway.app";
 const ENDPOINT = `${BASE_URL}/users`;
 
 document.getElementById("baseUrlDisplay").textContent = BASE_URL;
